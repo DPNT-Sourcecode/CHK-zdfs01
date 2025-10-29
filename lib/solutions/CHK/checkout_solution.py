e095bb77efa55d3593ff89e83eb23bb844de960a
sbx_user1051 <sbx_user1051@169.254.3.53>
2025-10-29 12:43:22 +0000

    Wed Oct 29 12:43:22 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,12 +8,36 @@ class CheckoutSolution:
         Calculate the total checkout price for the given SKU string.
 
         Pricing and offers:
-        - A: 50 each, 3A for 130, 5A for 200
-        - B: 30 each, 2B for 45
-        - C: 20 each
-        - D: 15 each
-        - E: 40 each, 2E gives one B free
-        - F: 10 each, 2F gives one F free
+        +------+-------+------------------------+
+        | Item | Price | Special offers         |
+        +------+-------+------------------------+
+        | A    | 50    | 3A for 130, 5A for 200 |
+        | B    | 30    | 2B for 45              |
+        | C    | 20    |                        |
+        | D    | 15    |                        |
+        | E    | 40    | 2E get one B free      |
+        | F    | 10    | 2F get one F free      |
+        | G    | 20    |                        |
+        | H    | 10    | 5H for 45, 10H for 80  |
+        | I    | 35    |                        |
+        | J    | 60    |                        |
+        | K    | 80    | 2K for 150             |
+        | L    | 90    |                        |
+        | M    | 15    |                        |
+        | N    | 40    | 3N get one M free      |
+        | O    | 10    |                        |
+        | P    | 50    | 5P for 200             |
+        | Q    | 30    | 3Q for 80              |
+        | R    | 50    | 3R get one Q free      |
+        | S    | 30    |                        |
+        | T    | 20    |                        |
+        | U    | 40    | 3U get one U free      |
+        | V    | 50    | 2V for 90, 3V for 130  |
+        | W    | 20    |                        |
+        | X    | 90    |                        |
+        | Y    | 10    |                        |
+        | Z    | 50    |                        |
+        +------+-------+------------------------+
 
         Args:
             skus: A string of SKU letters representing items in the basket
@@ -69,6 +93,7 @@ class CheckoutSolution:
         total += d *15  
 
         return total
+
 
 
 
