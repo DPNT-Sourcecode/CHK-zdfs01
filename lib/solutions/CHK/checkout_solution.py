f27a151784393fb0f08a89b1b2d3944840d8f303
sbx_user1051 <sbx_user1051@169.254.3.53>
2025-10-29 12:17:27 +0000

    Wed Oct 29 12:17:27 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,20 +4,34 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus: str | None) -> int:
+        """This function does XYZ"""
+
         # Reject non strings
         if not isinstance(skus, str):
             return -1 
         # Reject illegal input
-        if any(ch not in "ABCD" for ch in skus):
+        if any(ch not in "ABCDE" for ch in skus):
             return -1
         
+
+
         counts = Counter(skus)
         a = counts.get("A", 0)
         b = counts.get("B", 0)
         c = counts.get("C", 0)
         d = counts.get("D", 0)
+        e = counts.get("E", 0)
 
         total = 0
+
+        # E offer: 2E get one B free
+
+
+
+        # A offer 5A for 200, 3A for 130, else 
+
+
+
 
         # Offers 
         total += (a // 3) * 130 + (a % 3) * 50 # A: 3 for 130, else 50 each
