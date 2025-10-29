040f5bfc6c96c75fa07bc8381283e6c24e1f4282
sbx_user1051 <sbx_user1051@169.254.3.53>
2025-10-29 12:00:42 +0000

    Wed Oct 29 12:00:42 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,11 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
+        # Reject non strings
         if not isinstance(skus, str):
             return -1 
-        if any
+        # Reject illegal input
+        if any(ch not in "ABCD" for ch in skus):
+            return -1
+
 
