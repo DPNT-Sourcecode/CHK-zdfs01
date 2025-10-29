71e1b8fb574f7f2b2dc6d191ea5abd9f09f23f55
sbx_user1051 <sbx_user1051@169.254.3.53>
2025-10-29 13:15:08 +0000

    Wed Oct 29 13:15:08 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,37 +8,36 @@ class CheckoutSolution:
         Calculate the total checkout price for the given SKU string.
 
         Pricing and offers:
-        +------+-------+------------------------+
-        | Item | Price | Special offers         |
-        +------+-------+------------------------+
-        | A    | 50    | 3A for 130, 5A for 200 |
-        | B    | 30    | 2B for 45              |
-        | C    | 20    |                        |
-        | D    | 15    |                        |
-        | E    | 40    | 2E get one B free      |
-        | F    | 10    | 2F get one F free      |
-        | G    | 20    |                        |
-        | H    | 10    | 5H for 45, 10H for 80  |
-        | I    | 35    |                        |
-        | J    | 60    |                        |
-        | K    | 80    | 2K for 150             |
-        | L    | 90    |                        |
-        | M    | 15    |                        |
-        | N    | 40    | 3N get one M free      |
-        | O    | 10    |                        |
-        | P    | 50    | 5P for 200             |
-        | Q    | 30    | 3Q for 80              |
-        | R    | 50    | 3R get one Q free      |
-        | S    | 30    |                        |
-        | T    | 20    |                        |
-        | U    | 40    | 3U get one U free      |
-        | V    | 50    | 2V for 90, 3V for 130  |
-        | W    | 20    |                        |
-        | X    | 90    |                        |
-        | Y    | 10    |                        |
-        | Z    | 50    |                        |
-        +------+-------+------------------------+
-
+        +------+-------+---------------------------------+
+        | Item | Price | Special offers                  |
+        +------+-------+---------------------------------+
+        | A    | 50    | 3A for 130, 5A for 200          |
+        | B    | 30    | 2B for 45                       |
+        | C    | 20    |                                 |
+        | D    | 15    |                                 |
+        | E    | 40    | 2E get one B free               |
+        | F    | 10    | 2F get one F free               |
+        | G    | 20    |                                 |
+        | H    | 10    | 5H for 45, 10H for 80           |
+        | I    | 35    |                                 |
+        | J    | 60    |                                 |
+        | K    | 70    | 2K for 120                      |
+        | L    | 90    |                                 |
+        | M    | 15    |                                 |
+        | N    | 40    | 3N get one M free               |
+        | O    | 10    |                                 |
+        | P    | 50    | 5P for 200                      |
+        | Q    | 30    | 3Q for 80                       |
+        | R    | 50    | 3R get one Q free               |
+        | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | U    | 40    | 3U get one U free               |
+        | V    | 50    | 2V for 90, 3V for 130           |
+        | W    | 20    |                                 |
+        | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        +------+-------+---------------------------------+
         Args:
             skus: A string of SKU letters representing items in the basket
         
@@ -153,8 +152,3 @@ class CheckoutSolution:
         total += counts.get("Z", 0) * 50
 
         return total
-
-
-
-
-
